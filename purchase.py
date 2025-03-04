485bad0a41457463b09d9e7f50cee633d5d5daf4
sugata <k.sugata.sys24@morijyobi.ac.jp>
2025-03-04 22:34:16 +0900

    2025-03-04の成果

--- purchase.py
+++ purchase.py
@@ -13,12 +13,14 @@ class Purchase(tk.Frame):
         self.product_price_list = product_price_list
         self.product_image_list = product_image_list
         self.image_list = []
-        self.product_list = []
-        self.point_list = [1, 2, 3, 5, 10, 100, 1000, 10000]
+        self.quantities = [1] * len(product_id_list)
+        self.current_page = 0
+        self.items_per_page = 4
         self.pack()
         master.geometry('400x500')
         master.title('購入')
         self.create_widgets()
+        self.update_page()
     def create_widgets(self):
         self.return_button = tk.Button(self, text='<', command=self.return_event)
         self.return_button.place(x=30, y=30)
@@ -26,156 +28,104 @@ class Purchase(tk.Frame):
         self.label1.place(x=180, y=30)
         self.label2 = tk.Label(self, text=f'商品の詳細({len(self.product_name_list)})')
         self.label2.place(x=30, y=80)
-        if len(self.product_image_list) < 5:
-            num = len(self.product_image_list)
-        else:
-            num = 4
-        for i in range(num):
-            img1 = Image.open(self.product_image_list[i])
-            img1 = img1.resize((50, 50))
-            product_img = ImageTk.PhotoImage(img1)
-            self.image_list.append(product_img)
-            self.product_image_label = tk.Label(self, image=product_img)
-            self.product_image_label.place(x=i * 60 + 30, y=100)
-            self.price_label = tk.Label(self, text=f'{self.product_price_list[i]}円', fg='orange')
-            self.price_label.place(x=i * 60 + 30, y=150)
-            self.product_list.append(self.product_price_list[i])
-        self.minus_button1 = tk.Button(self, text='-', command=self.minus_event1, bg='gray')
-        self.minus_button1.place(x=30, y=180)
-        self.label3 = tk.Label(self, text=1)
-        self.label3.place(x=50, y=180)
-        self.plus_button1 = tk.Button(self, text='+', command=self.plus_event1, bg='gray')
-        self.plus_button1.place(x=70, y=180)
-        if num > 1:
-            self.minus_button2 = tk.Button(self, text='-', bg='gray', command=self.minus_event2)
-            self.minus_button2.place(x=90, y=180)
-            self.label4 = tk.Label(self, text=1)
-            self.label4.place(x=110, y=180)
-            self.plus_button2 = tk.Button(self, text='+', bg='gray', command=self.plus_event2)
-            self.plus_button2.place(x=130, y=180)
-        if num > 2:
-            self.minus_button3 = tk.Button(self, text='-', bg='gray', command=self.minus_event3)
-            self.minus_button3.place(x=150, y=180)
-            self.label5 = tk.Label(self, text=1)
-            self.label5.place(x=170, y=180)
-            self.plus_button3 = tk.Button(self, text='+', bg='gray', command=self.plus_event3)
-            self.plus_button3.place(x=190, y=180)
-        if num == 4:
-            self.minus_button4 = tk.Button(self, text='-', bg='gray', command=self.minus_event4)
-            self.minus_button4.place(x=210, y=180)
-            self.label6 = tk.Label(self, text=1)
-            self.label6.place(x=230, y=180)
-            self.plus_button4 = tk.Button(self, text='-', bg='gray', command=self.plus_event4)
-        self.label10 = tk.Label(self, text='ポイントで支払う')
-        self.label10.place(x=50, y=230)
-        self.label11 = tk.Label(self, text=f'{self.account[6]}pt', fg='gray')
-        self.label11.place(x=50, y=250)
-        self.check_status = tk.BooleanVar()
-        self.check1 = tk.Checkbutton(self, variable=self.check_status)
-        self.check1.place(x=100, y=250)
+        self.product_images = []
+        self.price_labels = []
+        self.quantity_labels = []
+        self.minus_buttons = []
+        self.plus_buttons = []
+        for i in range(self.items_per_page):
+            product_img_label = tk.Label(self)
+            product_img_label.place(x=30, y=100 + i * 80)
+            self.product_images.append(product_img_label)
+            price_label = tk.Label(self, fg='orange')
+            price_label.place(x=i * 60 + 30, y=150)
+            self.price_labels.append(price_label)
+            minus_button = tk.Button(self, text='-', bg='gray', command=lambda idx=i: self.update_quantity(idx, -1))
+            minus_button.place(x=i * 60 + 40, y=180)
+            self.minus_buttons.append(minus_button)
+            quantity_label = tk.Label(self, text="1")
+            quantity_label.place(x=i * 60 + 60, y=180)
+            self.quantity_labels.append(quantity_label)
+            plus_button = tk.Button(self, text='+', bg='gray', command=lambda idx=i: self.update_quantity(idx, 1))
+            plus_button.place(x=i * 60 + 80, y=180)
+            self.plus_buttons.append(plus_button)
+        self.prev_button = tk.Button(self, text='前へ', command=self.prev_page, state=tk.DISABLED)
+        self.prev_button.place(x=100, y=350)
+        self.next_button = tk.Button(self, text='次へ', command=self.next_page)
+        self.next_button.place(x=250, y=350)
         self.label7 = tk.Label(self, text='総額')
         self.label7.place(x=30, y=400)
-        self.label8 = tk.Label(self, text=sum(self.product_list), font=('', 13, 'bold'))
+        self.label8 = tk.Label(self, text='0', font=('', 13, 'bold'))
         self.label8.place(x=60, y=400)
         self.label9 = tk.Label(self, text='円', font=('', 8))
         self.label9.place(x=130, y=405)
         self.confirmed_button = tk.Button(self, text='注文を確定', fg='white', bg='orange', command=self.confirmed_event)
         self.confirmed_button.place(x=300, y=400)
+    def update_page(self):
+        start_index = self.current_page * self.items_per_page
+        end_index = start_index + self.items_per_page
+        self.image_list.clear()
+        for i in range(self.items_per_page):
+            idx = start_index + i
+            if idx < len(self.product_image_list):
+                img = Image.open(self.product_image_list[idx])
+                img = img.resize((50, 50))
+                product_img = ImageTk.PhotoImage(img)
+                self.image_list.append(product_img)
+                self.product_images[i].config(image=product_img)
+                self.price_labels[i].config(text=f'{self.product_price_list[idx]}円')
+                self.quantity_labels[i].config(text=str(self.quantities[idx]))
+                self.minus_buttons[i].config(state=tk.NORMAL)
+                self.plus_buttons[i].config(state=tk.NORMAL)
+            else:
+                self.product_images[i].config(image=None)
+                self.price_labels[i].config(text='')
+                self.quantity_labels[i].config(text='')
+                self.minus_buttons[i].config(state=tk.DISABLED)
+                self.plus_buttons[i].config(state=tk.DISABLED)
+        self.prev_button.config(state=tk.NORMAL if self.current_page > 0 else tk.DISABLED)
+        self.next_button.config(state=tk.NORMAL if end_index < len(self.product_image_list) else tk.DISABLED)
+        self.update_total_price()
+    def update_quantity(self, button_index, change):
+        product_index = self.current_page * self.items_per_page + button_index
+        if 0 <= product_index < len(self.quantities):
+            new_quantity = self.quantities[product_index] + change
+            if new_quantity > 0:
+                self.quantities[product_index] = new_quantity
+                self.quantity_labels[button_index].config(text=str(new_quantity))
+                self.update_total_price()
+            else:
+                response = messagebox.askyesno('カートから削除', 'この商品をカートから削除しますか?')
+                if response:
+                    self.quantities[product_index] = 0
+                    self.quantity_labels[button_index].config(text="×")
+                    self.minus_buttons[button_index].config(state=tk.DISABLED)
+                    self.plus_buttons[button_index].config(state=tk.DISABLED)
+                    self.update_total_price()
+    def update_total_price(self):
+        total_price = sum(self.product_price_list[i] * self.quantities[i] for i in range(len(self.product_price_list)))
+        self.label8.config(text=f'{total_price}')
+    def prev_page(self):
+        if self.current_page > 0:
+            self.current_page -= 1
+            self.update_page()
+    def next_page(self):
+        if (self.current_page + 1) * self.items_per_page < len(self.product_image_list):
+            self.current_page += 1
+            self.update_page()
     def return_event(self):
         from search import Search
         self.destroy()
         Search(self.master, self.account)
-    def minus_event1(self):
-        product_num1 = self.label3.cget('text')
-        if product_num1 > 1:
-            self.label3.configure(text=product_num1 - 1)
-            self.product_list[0] -= self.product_price_list[0]
-            self.label8.configure(text=sum(self.product_list))
-        else:
-            response = messagebox.askyesno('カートから削除', 'この商品をカートから削除しますか?')
-            if response:
-                self.label3.configure(text='×')
-                self.minus_button1.configure(state='disabled')
-                self.plus_button1.configure(state='disabled')
-                self.product_list[0] = 0
-                self.label8.configure(text=sum(self.product_list))
-    def plus_event1(self):
-        product_num1 = self.label3.cget('text')
-        self.label3.configure(text=product_num1 + 1)
-        self.product_list[0] += self.product_price_list[0]
-        self.label8.configure(text=sum(self.product_list))
-    def minus_event2(self):
-        product_num2 = self.label4.cget('text')
-        if product_num2 > 1:
-            self.label4.configure(text=product_num2 - 1)
-            self.product_list[1] -= self.product_price_list[1]
-            self.label8.configure(text=sum(self.product_list))
-        else:
-            response = messagebox.askyesno('カートから削除', 'この商品をカートから削除しますか?')
-            if response:
-                self.label4.configure(text='×')
-                self.minus_button2.configure(state='disabled')
-                self.plus_button2.configure(state='disabled')
-                self.product_list[1] = 0
-                self.label8.configure(text=sum(self.product_list))
-    def plus_event2(self):
-        product_num2 = self.label4.cget('text')
-        self.label4.configure(text=product_num2 + 1)
-        self.product_list[1] += self.product_price_list[1]
-        self.label8.configure(text=sum(self.product_list))
-    def minus_event3(self):
-        product_num3 = self.label5.cget('text')
-        if product_num3 > 1:
-            self.label5.configure(text=product_num3 - 1)
-            self.product_list[2] -= self.product_price_list[2]
-            self.label8.configure(text=sum(self.product_list))
-        else:
-            response = messagebox.askyesno('カートから削除', 'この商品をカートから削除しますか?')
-            if response:
-                self.label5.configure(text='×')
-                self.minus_button3.configure(state='disabled')
-                self.plus_button3.configure(state='disabled')
-                self.product_list[2] = 0
-                self.label8.configure(text=sum(self.product_list))
-    def plus_event3(self):
-        product_num3 = self.label5.cget('text')
-        self.label5.configure(text=product_num3 + 1)
-        self.product_list[2] += self.product_price_list[2]
-        self.label8.configure(text=sum(self.product_list))
-    def minus_event4(self):
-        product_num4 = self.label6.cget('text')
-        if product_num4 > 1:
-            self.label6.configure(text=product_num4 - 1)
-            self.product_list[3] -= self.product_price_list[3]
-            self.label8.configure(text=sum(self.product_list))
-        else:
-            response = messagebox.askyesno('カートから削除', 'この商品をカートから削除しますか?')
-            if response:
-                self.label6.configure(text='×')
-                self.minus_button4.configure(state='disabled')
-                self.plus_button4.configure(state='disabled')
-                self.product_list[3] = 0
-                self.label8.configure(text=sum(self.product_list))
-    def plus_event4(self):
-        product_num4 = self.label6.cget('text')
-        self.label6.configure(product_num4 + 1)
-        self.product_list[3] += self.product_price_list[3]
-        self.label8.configure(text=sum(self.product_list))
     def confirmed_event(self):
-        total = sum(self.product_list)
+        total = sum(self.product_price_list[i] * self.quantities[i] for i in range(len(self.product_price_list)))
         insert_orders(self.account[0], total)
         row = select_orders()
-        for i in range(len(self.product_list)):
-            if self.product_list[i] != 0:
-                insert_order_details(row[0], self.product_id_list[i], self.product_list[i] // self.product_price_list[i], self.product_list[i])
-        if self.check_status.get():
-            new_point = self.account[6] - total
-            if new_point < 0:
-                new_point = 0
-            update_point(new_point, self.account[0])
-            self.account = get_account_by_email(self.account[4])
+        for i in range(len(self.product_id_list)):
+            if self.quantities[i] > 0:
+                insert_order_details(row[0], self.product_id_list[i], self.quantities[i], self.product_price_list[i] * self.quantities[i])
         if total >= 10000:
-            point = random.choice(self.point_list)
+            point = random.choice([1, 2, 3, 5, 10, 100, 1000, 10000])
             new_point = self.account[6] + point
             update_point(new_point, self.account[0])
             self.account = get_account_by_email(self.account[4])
